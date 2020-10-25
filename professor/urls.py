a1e86a593c0fb32c9b6f81f2fb3c66d67a9c7791
page-not-found <hitalfashion@gmail.com>
2020-10-25 20:56:14 +0530

    unwanted file removed

--- professor/urls.py
+++ professor/urls.py
@@ -1,10 +1,1 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
 
-urlpatterns = [
-    path('login', views.handellogin, name='handellogin'),
-    path('', views.dashboard, name='dashboard'),
-    path('logout', views.handellogout, name='handellogout'),
-    # path('result-upload', views.resultupload, name='resultupload'),
-]
