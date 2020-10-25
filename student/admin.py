1f2116d5d2f25ec91507b68592184292449a9120
page-not-found <hitalfashion@gmail.com>
2020-10-25 21:51:50 +0530

    in model result system added

--- student/admin.py
+++ student/admin.py
@@ -8,27 +8,33 @@ from import_export.admin import ImportExportModelAdmin
 class FirstAdmin(ImportExportModelAdmin):
     resource_class = FirstResource
     list_display = ('roll_no','student_name','enrollment_no','semester','sub_1','sub_2','sub_3',
-        'sub_4','sub_5','sub_6',)
+        'sub_4','sub_5','sub_6','total_marks','percentage','result')
     list_display_links = ('roll_no','student_name',)
+    readonly_fields  = ('percentage','total_marks','result')
     search_fields = ('roll_no','student_name','enrollment_no')
+    list_filter = ('result',)
     list_per_page = 50
 
 @admin.register(second_year)
 class SecoundAdmin(ImportExportModelAdmin):
     resource_class = SecoundResource
     list_display = ('roll_no','student_name','enrollment_no','semester','sub_1','sub_2','sub_3',
-        'sub_4','sub_5','sub_6',)
+        'sub_4','sub_5','sub_6','total_marks','percentage','result')
     list_display_links = ('roll_no','student_name',)
+    readonly_fields  = ('percentage','total_marks','result')
     search_fields = ('roll_no','student_name','enrollment_no')
+    list_filter = ('result',)
     list_per_page = 50
 
 @admin.register(third_year)
 class ThirdAdmin(ImportExportModelAdmin):
     resource_class = ThirdResource
     list_display = ('roll_no','student_name','enrollment_no','semester','sub_1','sub_2','sub_3',
-        'sub_4','sub_5','sub_6',)
+        'sub_4','sub_5','sub_6','total_marks','percentage','result')
     list_display_links = ('roll_no','student_name',)
+    readonly_fields  = ('percentage','total_marks','result')
     search_fields = ('roll_no','student_name','enrollment_no')
+    list_filter = ('result',)
     list_per_page = 50
     
 admin.site.register(Csv)
--- student/models.py
+++ student/models.py
@@ -16,7 +16,14 @@ class first_year(models.Model):
     sub_4 = models.IntegerField(default=0)
     sub_5 = models.IntegerField(default=0)
     sub_6 = models.IntegerField(default=0)
+    total_marks = models.IntegerField(default=0)
+    percentage = models.FloatField(default=0)
+    result = models.CharField(max_length=4, default="FAIL")
     timeStamp = models.DateTimeField(default=now, blank=True)
+
+    def save(self, *args, **kwargs):
+        pass
+        super.save(*args, **kwargs)
 
     def __str__(self):
         return str(self.roll_no) + " " + self.student_name
@@ -33,7 +40,14 @@ class second_year(models.Model):
     sub_4 = models.IntegerField(default=0)
     sub_5 = models.IntegerField(default=0)
     sub_6 = models.IntegerField(default=0)
+    total_marks = models.IntegerField(default=0)
+    percentage = models.FloatField(default=0)
+    result = models.CharField(max_length=4, default="FAIL")
     timeStamp = models.DateTimeField(default=now, blank=True)
+
+    def save(self, *args, **kwargs):
+        pass
+        super.save(*args, **kwargs)
 
     def __str__(self):
         return str(self.roll_no) + " " + self.student_name
@@ -50,7 +64,14 @@ class third_year(models.Model):
     sub_4 = models.IntegerField(default=0)
     sub_5 = models.IntegerField(default=0)
     sub_6 = models.IntegerField(default=0)
+    total_marks = models.IntegerField(default=0)
+    percentage = models.FloatField(default=0)
+    result = models.CharField(max_length=4, default="FAIL")
     timeStamp = models.DateTimeField(default=now, blank=True)
+
+    def save(self, *args, **kwargs):
+        pass
+        super.save(*args, **kwargs)
 
     def __str__(self):
         return str(self.roll_no) + " " + self.student_name
